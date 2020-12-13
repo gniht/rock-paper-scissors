60a4fdf73b992e5d1c2867e28f112b193b629f93
Jason Thomas <jasonthomasjat@gmail.com>
2020-12-12 22:52:59 -0800

    added 'playRound' function to return an appropriate outcome message based on player and computer selections for the round

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function computerPlay(){
   let num = Math.floor(Math.random()*3);
   let result = "";
@@ -9,4 +10,43 @@ function computerPlay(){
     result = "scissors";
   }
   return result;
+}
+
+function playRound(playerSelection, computerSelection){
+  let result, winner, loser, descriptor;
+  const resultMessage = `You ${result}! ${winner} ${descriptor} ${loser}`
+  if(playerSelection.toLowerCase() == computerSelection){
+    result = "tie";
+    winner = "Nobody";
+    descriptor = "likes";
+    loser = "a stalemate!";
+  }
+  if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
+    winner = playerSelection;
+    descriptor = "smashes";
+    loser = computerSelection;
+  }else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
+    winner = computerSelection;
+    descriptor = "encases";
+    loser = playerSelection;
+  }
+  if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
+    winner = playerSelection;
+    descriptor = "encases";
+    loser = computerSelection;        
+  }else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"){
+    winner = computerSelection;
+    descriptor = "cut";
+    loser = playerSelection; 
+  }
+  if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"){
+    winner = playerSelection;
+    descriptor = "cut";
+    loser = computerSelection;    
+  }else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"){
+    winner = computerSelection;
+    descriptor = "smashes";
+    loser = playerSelection;
+  }
+  return resultMessage;  
 }
