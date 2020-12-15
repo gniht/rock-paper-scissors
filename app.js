83ee9410d38d83d24deae5caef1662f2b6f77a81
Jason Thomas <jasonthomasjat@gmail.com>
2020-12-15 14:43:19 -0800

    refined ui adding scoreboard, outcome message, and a reset function with timeout

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 
+let score = {playerScore: 0, computerScore: 0};
+
 const gameWindow = document.createElement("div");
 gameWindow.classList.add("container");
 const banner = document.createElement("h1");
 gameWindow.append(banner);
 const selection = document.createElement("div");
+selection.classList.add("centerh");
+const startdiv = document.createElement("div");
+
+const startBtn = document.createElement("button");
+startBtn.classList.add("button", "centered");
+const startCaption = document.createElement("h3");
+startCaption.textContent = `Zero margin for error. One challenger. One enemy. Two combatants. Three weapons. Five victories.`;
+startCaption.classList.add("centerh");
+startBtn.textContent = "Make Fibonacci Proud";
+startdiv.append(startCaption);
+startdiv.append(startBtn);
+gameWindow.append(startdiv);
 const rockButton = document.createElement("button");
 rockButton.innerText = "rock";
 rockButton.classList.add("button");
@@ -17,11 +31,44 @@ selection.append(rockButton);
 selection.append(paperButton);
 selection.append(scissorsButton);
 banner.innerText = "Rock - Paper - Scissors";
-gameWindow.append(selection);
+const scoreBoard = document.createElement("h2");
+scoreBoard.innerText = `Human: ${score.playerScore} || Computer: ${score.computerScore}`;
+scoreBoard.classList.add("centerh");
+
 document.querySelector("body").append(gameWindow);
+
+function reset(){
+  gameWindow.removeChild(scoreBoard);  
+  gameWindow.removeChild(selection);
+  gameWindow.append(startdiv);
+  score.playerScore = 0;
+  score.computerScore = 0;
+  scoreBoard.innerText = `Human: ${score.playerScore} || Computer: ${score.computerScore}`; 
+}
+startBtn.addEventListener("click", e =>{
+  gameWindow.removeChild(startdiv);
+  if(e.target.textContent == "Make Fibonacci Proud"){
+    playerScore = 0;
+    computerScore = 0;
+  }
+  gameWindow.append(scoreBoard);  
+  gameWindow.append(selection);
+  
+});
 selection.addEventListener("click", e => {
-  const playerSelection = e.target.innerText;
-  alert(playRound(playerSelection, computerPlay()));  
+  const playerSelection = e.target.innerText;    
+  playRound(playerSelection, computerPlay(), score);
+  if(score.playerScore >= 5){
+    scoreBoard.innerText = "You have emerged victorious!";
+    setTimeout(reset, 3000);
+  }else if (score.computerScore >= 5){
+    scoreBoard.innerText = "You have been defeated!";
+    setTimeout(reset, 3000);
+  }else{
+    scoreBoard.innerText = `Human: ${score.playerScore} || Computer: ${score.computerScore}`;
+  }
+  
+  
 });
 
 function computerPlay(){
@@ -37,7 +84,7 @@ function computerPlay(){
   return result;
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection, score){
   let result, winner, loser, descriptor;
    
   if(playerSelection.toLowerCase() == computerSelection){
@@ -48,33 +95,39 @@ function playRound(playerSelection, computerSelection){
   }
   if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
     result = "win";
+    score.playerScore += 1;
     winner = playerSelection;
     descriptor = "smashes";
     loser = computerSelection;
   }else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
     result = "lose";
+    score.computerScore += 1;
     winner = computerSelection;
     descriptor = "encases";
     loser = playerSelection;
   }
   if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
     result = "win";
+    score.playerScore += 1;
     winner = playerSelection;
     descriptor = "encases";
     loser = computerSelection;        
   }else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"){
     result = "lose";
+    score.computerScore += 1;
     winner = computerSelection;
     descriptor = "cut";
     loser = playerSelection; 
   }
   if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"){
     result = "win";
+    score.playerScore += 1;
     winner = playerSelection;
     descriptor = "cut";
     loser = computerSelection;    
   }else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"){
     result = "lose";
+    score.computerScore += 1;
     winner = computerSelection;
     descriptor = "smashes";
     loser = playerSelection;
@@ -82,17 +135,17 @@ function playRound(playerSelection, computerSelection){
   return `You ${result}! ${winner} ${descriptor} ${loser}`;  
 }
 
-function game(){
-  //todo
-  let playerScore = 0;
-  let computerScore = 0;
-  let gameOver = (playerScore >= 5 || computerScore >= 5);
-  while(!gameOver){
-    playerSelection = prompt("Select your weapon.");
-    computerSelection = computerPlay();
-    alert(playRound(playerSelection, computerSelection));
-  }
-}
+
+// function game(){//   
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   let gameOver = (playerScore >= 5 || computerScore >= 5);
+//   while(!gameOver){
+//     playerSelection = prompt("Select your weapon.");
+//     computerSelection = computerPlay();
+//     alert(playRound(playerSelection, computerSelection));
+//   }
+// }
 
 
 
